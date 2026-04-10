5bb74676a12e521fe1f0c458841f8831c4eab09c
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-04-10 11:16:24 +0100

    Test by setting affected blocks to diamond

--- src/main/java/dev/perxenic/explosionoverhaul/ExplosionOverhaul.java
+++ src/main/java/dev/perxenic/explosionoverhaul/ExplosionOverhaul.java
@@ -1,5 +1,9 @@
 package dev.perxenic.explosionoverhaul;
 
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.level.ExplosionEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -9,13 +13,22 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 
+@EventBusSubscriber
 @Mod(ExplosionOverhaul.MODID)
 public class ExplosionOverhaul {
     public static final String MODID = "explosionoverhaul";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public ExplosionOverhaul(ModContainer modContainer) {
-
         modContainer.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
+    }
+
+    @SubscribeEvent
+    public static void onExplosionDetonate(ExplosionEvent.Detonate event) {
+        for (var pos : event.getAffectedBlocks()) {
+            event.getLevel().setBlock(pos, Blocks.DIAMOND_BLOCK.defaultBlockState(), 0b0000011);
+        }
+
+        event.getAffectedBlocks().clear();
     }
 }
