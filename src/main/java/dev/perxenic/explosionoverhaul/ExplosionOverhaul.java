babcdc837b142a5c1ea59727cfe76e436b55571b
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-04-10 13:40:00 +0100

    Add basic block scattering

--- src/main/java/dev/perxenic/explosionoverhaul/ExplosionOverhaul.java
+++ src/main/java/dev/perxenic/explosionoverhaul/ExplosionOverhaul.java
@@ -2,9 +2,14 @@ package dev.perxenic.explosionoverhaul;
 
 import dev.perxenic.explosionoverhaul.content.EOTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.ExplosionEvent;
@@ -18,6 +23,7 @@ import net.neoforged.fml.ModContainer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @EventBusSubscriber
 @Mod(ExplosionOverhaul.MODID)
@@ -35,14 +41,16 @@ public class ExplosionOverhaul {
 
     @SubscribeEvent
     public static void onExplosionDetonate(ExplosionEvent.Detonate event) {
-        var level = event.getLevel();
+        if (!(event.getLevel() instanceof ServerLevel level)) return;
+
         var explosion = event.getExplosion();
+        var center = explosion.center();
 
         // Prevent explosions which do not break blocks from triggering effects
         if (explosion.getBlockInteraction() == Explosion.BlockInteraction.KEEP) return;
         if (explosion.getBlockInteraction() == Explosion.BlockInteraction.TRIGGER_BLOCK) return;
 
-        // Create a list of all blocks that have been processed by Explosion Overhaul
+        // Create a list of all blocks that have been processed by Explosion Overhaul to remove from vanilla processing
         List<BlockPos> processedBlocks = new ArrayList<>();
 
         for (var pos : event.getAffectedBlocks()) {
@@ -51,8 +59,20 @@ public class ExplosionOverhaul {
             // Do not process blocks tagged to not launch
             if (blockState.is(EOTags.Blocks.DO_NOT_LAUNCH)) continue;
 
+            var posVec = new Vec3(pos.getX(), pos.getY(), pos.getZ());
+            var difference = posVec.subtract(center);
+            var distance = difference.length();
+
+            // Do not process blocks incredibly close to explosion center
+            if (distance < 1e-6) continue;
+
+            var direction = difference.normalize();
+
+            var fallingBlock = FallingBlockEntity.fall(level, pos, blockState);
             processedBlocks.add(pos);
-            event.getLevel().setBlock(pos, Blocks.DIAMOND_BLOCK.defaultBlockState(), 0b0000011);
+
+            fallingBlock.push(direction);
+            level.getServer().getPlayerList().broadcastAll(new ClientboundSetEntityMotionPacket(fallingBlock.getId(), fallingBlock.getDeltaMovement()));
         }
 
         // Remove all blocks from explosion handling that have been processed by Explosion Overhaul
